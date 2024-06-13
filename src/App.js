6514f12a1c7fa5074b8e02a99e5628b7c640409b
luan1412167 <luan1412167@gmail.com>
2024-06-13 08:35:01 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 function App() {
+
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="mdc-top-app-bar">
+      <div className="mdc-top-app-bar__row">
+        <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+          <a href="/sol/" className="router-link-active router-link-exact-active mdc-top-app-bar__logo-container" aria-current="page">
+            <img src="/img/bubblemaps.51902376.svg" alt="Bubblemaps Logo" className="mdc-top-app-bar__logo"/>
+          </a>
+        <Select value={selectedValue} onChange={handleChange} className='mySelect'>
+        <MenuItem value={'option1'}>SOL</MenuItem>
+        <MenuItem value={'option2'}>ETH</MenuItem>
+        <MenuItem value={'option3'}>BTC</MenuItem>
+        </Select>
+        </section>
+        <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+          <div className="mdc-text-field mdc-text-field--outlined mdc-text-field--with-leading-icon">
+            <i className="material-icons mdc-text-field__icon" tabindex="0" aria-hidden="true">search</i>
+            <input type="text" id="search-field" className="mdc-text-field__input" />
+            <div className="mdc-notched-outline">
+              <div className="mdc-notched-outline__leading"></div>
+              <div className="mdc-notched-outline__notch">
+                <label for="search-field" className="mdc-floating-label">Search</label>
+              </div>
+              <div className="mdc-notched-outline__trailing"></div>
+            </div>
+          </div>
+          <button className="material-icons mdc-top-app-bar__action-item mdc-icon-button" aria-label="Login">login</button>
+        </section>
+      </div>
+    </header>
   );
 }
 
